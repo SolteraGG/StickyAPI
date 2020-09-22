9362ee73b694eaf9088c32c766cc8998f9af4636
Zachery <zachery@foxboys.online>
2020-09-22 15:39:41 -0500

    Added command and statup utils

---
+++ src/main/java/com/ristexsoftware/koffee/bukkit/util/CommandUtil.java
@@ -0,0 +1,74 @@
+package com.ristexsoftware.koffee.bukkit.util;
+
+import java.util.List;
+
+import com.ristexsoftware.koffee.util.ReflectionUtil;
+
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
+
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ScanResult;
+
+public class CommandUtil {
+    /**
+     * Register a command with bukkit
+     * @param server The bukkit server
+     * @param command The command to register
+     * @return False if something went wrong
+     */
+    public boolean registerCommand(Server server, Command command) {
+        try {
+            CommandMap cmap = ReflectionUtil.getProtectedValue(server, "commandMap");
+            cmap.register(server.getName().toLowerCase(), command);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Register a {@link java.util.List} of commands with bukkit
+     * @param server The bukkit server
+     * @param commands The command to register
+     * @return False if something went wrong
+     */
+    public boolean registerCommands(Server server, List<Command> commands) {
+        try {
+            CommandMap cmap = ReflectionUtil.getProtectedValue(server, "commandMap");
+            cmap.registerAll(server.getName().toLowerCase(), commands);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Register all commands in a specific package
+     * @param server The bukkit server
+     * @param pkg The package path (e.g. com.ristexsoftware.knappy.commands)
+     * @return False if something went horribly wrong
+     * @deprecated This method uses reflection and is prone to error in later Java versions
+     */
+    public boolean registerCommandPackage(Server server, String pkg) {
+        List<String> classNames;
+        try (ScanResult scanResult = new ClassGraph().acceptPackages(pkg).enableClassInfo().scan()) {
+            classNames = scanResult.getAllClasses().getNames();
+        }
+        try {
+            for (String className : classNames) {
+                Class<?> clazz = Class.forName(pkg+"."+className);
+                Object command = clazz.newInstance();
+                CommandMap cmap = ReflectionUtil.getProtectedValue(server, "commandMap");
+                cmap.register(server.getName().toLowerCase(), (Command)command);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
--- src/main/java/com/ristexsoftware/koffee/bukkit/util/StartupUtil.java
+++ src/main/java/com/ristexsoftware/koffee/bukkit/util/StartupUtil.java
@@ -1,11 +1,15 @@
 package com.ristexsoftware.koffee.bukkit.util;
 
+import java.io.File;
+import com.ristexsoftware.koffee.translation.LocaleProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StartupUtil {
 
     /**
-     * Generate a configuration datafolder and save the default config if one does not exist
+     * Generate a configuration datafolder and save the default config if one does
+     * not exist
+     * 
      * @param plugin The plugin's main class
      * @return False if something went wrong
      */
@@ -25,6 +29,35 @@ public class StartupUtil {
         }
     }
 
-    
-    
+    /**
+     * Load the server's localizations
+     * 
+     * @param plugin         The plugin's main class
+     * @param localeProvider The plugin's locale provider
+     * @return False if something went wrong
+     */
+    public boolean setupLocale(JavaPlugin plugin, LocaleProvider localeProvider) {
+        localeProvider = new LocaleProvider(new File(plugin.getDataFolder(), "locale"));
+        int loadedLocales = localeProvider.loadAllLocales();
+        Boolean localeEnabled = localeProvider.setDefaultLocale("messages.en_us");
+
+        if (!localeEnabled) {
+            plugin.getLogger()
+                    .severe("Failed to configure default locale file - perhaps you deleted it? Will create a new one.");
+            // FIXME: This is horrible and needs to be improved
+            try {
+                localeProvider.writeLocaleStream(plugin.getResource("messages.en_us.yml"), "messages.en_us.yml", true);
+            } catch (Exception e) {
+                e.printStackTrace();
+                plugin.getLogger().severe("Something went horribly wrong while saving the default locale.");
+                return false;
+            }
+
+            localeProvider.loadAllLocales();
+            localeProvider.setDefaultLocale("messages.en_us");
+        } else
+            plugin.getLogger().info("Loaded " + String.valueOf(loadedLocales) + " localizations");
+
+        return true;
+    }
 }
