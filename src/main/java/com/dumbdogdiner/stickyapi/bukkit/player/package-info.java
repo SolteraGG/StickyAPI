ff951b3c9166479c46c76c709af0a292de20fb12
James Cahill <jcahill773@gmail.com>
2020-11-10 20:23:04 +0000

    Run the license formatter on the source code

--- src/main/java/com/dumbdogdiner/stickyapi/bukkit/player/package-info.java
+++ src/main/java/com/dumbdogdiner/stickyapi/bukkit/player/package-info.java
@@ -1,4 +1,8 @@
 /**
+ * Copyright (c) 2020 DumbDogDiner <dumbdogdiner.com>. All rights reserved.
+ * Licensed under the GPLv3 license, see LICENSE for more information...
+ */
+/**
  * Classes dedicated to interacting with {@link org.bukkit.entity.Player}
  */
 package com.dumbdogdiner.stickyapi.bukkit.player;
