77c9103d4fdab114814a51dc2718acb272530624
James Cahill <jcahill773@gmail.com>
2020-11-11 08:52:37 +0000

    Automatic License Header via Gradle (#19)
    * Add automatic license header gradle plugin; create LICENSE_HEADER file; add licence-header-check ci job
    
    * Run the license formatter on the source code
    
    * (ci): Move 'license-header-check' job into 'ci' workflow

--- src/main/java/com/dumbdogdiner/stickyapi/bukkit/generator/package-info.java
+++ src/main/java/com/dumbdogdiner/stickyapi/bukkit/generator/package-info.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2020 DumbDogDiner <dumbdogdiner.com>. All rights reserved.
+ * Licensed under the GPLv3 license, see LICENSE for more information...
+ */
 
 /**
  * Classes dedicated to Minecraft world generation
--- src/main/java/com/dumbdogdiner/stickyapi/common/configuration/file/FileConfiguration.java
+++ src/main/java/com/dumbdogdiner/stickyapi/common/configuration/file/FileConfiguration.java
@@ -1,21 +1,7 @@
-/* 
- *  StickyAPI - Utility methods, classes and potentially code-dupe-annihilating code for DDD plugins
- *  Copyright (C) 2020 DumbDogDiner <dumbdogdiner.com>
- *  
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/**
+ * Copyright (c) 2020 DumbDogDiner <dumbdogdiner.com>. All rights reserved.
+ * Licensed under the GPLv3 license, see LICENSE for more information...
  */
-
 package com.dumbdogdiner.stickyapi.common.configuration.file;
 
 import com.google.common.base.Charsets;
