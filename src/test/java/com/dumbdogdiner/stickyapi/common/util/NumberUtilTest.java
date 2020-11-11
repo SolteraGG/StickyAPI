e25256c8d944535f92160deeceb720afaeae12a8
James Cahill <jcahill773@gmail.com>
2020-11-11 20:20:12 +0000

    Add some NumberUtil#intHelper tests

--- src/test/java/com/dumbdogdiner/stickyapi/common/util/NumberUtilTest.java
+++ src/test/java/com/dumbdogdiner/stickyapi/common/util/NumberUtilTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.RepeatedTest;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NumberUtilTest {
     // TODO: add getPercentage tests.
@@ -39,5 +40,22 @@ public class NumberUtilTest {
         assertThrows(IllegalArgumentException.class, () -> {
             NumberUtil.getRandomNumber(1, 0);
         });
+    }
+
+    // TODO: intHelper case 0 and default case tests?
+
+    @Test
+    public void testIntHelper() {
+        assertEquals(NumberUtil.intHelper(1L), 1);
+    }
+
+    @Test
+    public void testIntHelperMax() {
+        assertEquals(NumberUtil.intHelper(((long)Integer.MAX_VALUE) + 1L), Integer.MAX_VALUE);
+    }
+
+    @Test
+    public void testIntHelperMin() {
+        assertEquals(NumberUtil.intHelper(((long) Integer.MIN_VALUE) - 1L), Integer.MIN_VALUE);
     }
 }
