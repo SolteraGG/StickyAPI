07ff27c4f19c4bc1bd1bb9675394e6edc41e4395
James Cahill <jcahill773@gmail.com>
2020-11-13 08:49:19 +0000

    (tests): Add license header to NotificationTypeTest

--- src/test/java/com/dumbdogdiner/stickyapi/bukkit/util/NotificationTypeTest.java
+++ src/test/java/com/dumbdogdiner/stickyapi/bukkit/util/NotificationTypeTest.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2020 DumbDogDiner <dumbdogdiner.com>. All rights reserved.
+ * Licensed under the GPLv3 license, see LICENSE for more information...
+ */
 package com.dumbdogdiner.stickyapi.bukkit.util;
 
 import static com.dumbdogdiner.stickyapi_tests_common.TestsCommon.superficialEnumCodeCoverage;
