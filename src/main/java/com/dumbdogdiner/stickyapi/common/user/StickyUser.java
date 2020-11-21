17e8812a42d6a39fcaa9506e5030764be558e593
Andrew Katz <aakatz3@gmail.com>
2020-11-21 01:56:25 -0500

    Update todos, create initial sticky user class, planning for future

---
+++ src/main/java/com/dumbdogdiner/stickyapi/common/user/StickyUser.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2020 DumbDogDiner <dumbdogdiner.com>. All rights reserved.
+ * Licensed under the MIT license, see LICENSE for more information...
+ */
+package com.dumbdogdiner.stickyapi.common.user;
+
+import java.util.UUID;
+
+public class StickyUser {
+    private UUID uid;
+}
