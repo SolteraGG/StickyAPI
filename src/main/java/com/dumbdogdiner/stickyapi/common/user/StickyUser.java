2313b3066a0b2eb85710493640979faa195d42ac
Andrew Katz <aakatz3@gmail.com>
2020-11-21 13:23:27 -0500

    Create base stickyuser, switch to waterfall

--- src/main/java/com/dumbdogdiner/stickyapi/common/user/StickyUser.java
+++ src/main/java/com/dumbdogdiner/stickyapi/common/user/StickyUser.java
@@ -4,8 +4,50 @@
  */
 package com.dumbdogdiner.stickyapi.common.user;
 
+import com.dumbdogdiner.stickyapi.webapis.MojangAPI;
+import lombok.Getter;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import java.time.Instant;
+import java.util.Map;
 import java.util.UUID;
 
 public class StickyUser {
+    @Getter
     private UUID uid;
+
+    @Getter
+    private String userName;
+
+
+    public StickyUser(Player p){
+        uid = p.getUniqueId();
+        userName = p.getName();
+    }
+
+    public StickyUser(ProxiedPlayer p){
+        uid = p.getUniqueId();
+        userName = p.getName();
+    }
+
+    public StickyUser(OfflinePlayer p){
+        uid = p.getUniqueId();
+        userName = p.getName();
+    }
+
+    public Map<String, Instant> getNameHistory(){
+        MojangAPI api = new MojangAPI(uid);
+        return api.getUsernameHistory();
+    }
+
+    public Player getAsBukkitPlayer(){
+        return Bukkit.getPlayer(uid);
+    }
+
+    public OfflinePlayer getAsOfflinePlayer(){
+        return Bukkit.getOfflinePlayer(uid);
+    }
 }
