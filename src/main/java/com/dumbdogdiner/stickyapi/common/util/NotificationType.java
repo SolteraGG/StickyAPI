a169315913440bfb04af3cb0d50b7dbef6369ea2
Andrew Katz <aakatz3@gmail.com>
2020-11-28 15:05:25 -0700

    Add ability to get NotificationType from ExitCode

--- src/main/java/com/dumbdogdiner/stickyapi/common/util/NotificationType.java
+++ src/main/java/com/dumbdogdiner/stickyapi/common/util/NotificationType.java
@@ -4,6 +4,9 @@
  */
 package com.dumbdogdiner.stickyapi.common.util;
 
+import com.dumbdogdiner.stickyapi.common.command.ExitCode;
+import org.jetbrains.annotations.Nullable;
+
 /**
  * An enum of possible notification types.
  */
@@ -11,5 +14,27 @@ public enum NotificationType {
     ERROR,
     INFO,
     QUIET,
-    SUCCESS
+    SUCCESS;
+
+    public NotificationType fromExitCode(@Nullable ExitCode code){
+        if(code == null)
+            return QUIET;
+        switch(code){
+            case EXIT_SUCCESS:
+                return SUCCESS;
+            case EXIT_INFO:
+                return INFO;
+            case EXIT_BAD_SENDER:
+            case EXIT_COOLDOWN:
+            case EXIT_ERROR:
+            case EXIT_ERROR_SILENT:
+            case EXIT_EXPECTED_ERROR:
+            case EXIT_INVALID_STATE:
+            case EXIT_INVALID_SYNTAX:
+            case EXIT_MUST_BE_PLAYER:
+            case EXIT_PERMISSION_DENIED:
+            default:
+                return ERROR;
+        }
+    }
 }
