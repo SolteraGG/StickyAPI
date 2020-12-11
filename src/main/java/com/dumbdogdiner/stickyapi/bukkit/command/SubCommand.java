a4862844a8e6f43739de025e4d50ec894c21e4fc
Matt <25674682+spazzylemons@users.noreply.github.com>
2020-12-11 13:07:04 -0500

    Add Javadoc to @SubCommand

--- src/main/java/com/dumbdogdiner/stickyapi/bukkit/command/SubCommand.java
+++ src/main/java/com/dumbdogdiner/stickyapi/bukkit/command/SubCommand.java
@@ -9,6 +9,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a <i>static</i> field of a command class as a subcommand of that command. The name and aliases of the
+ * subcommand become the flags that trigger the subcommand.
+ * <br/>
+ * Example:
+ * <pre>{@code
+ * class FooCommand extends StickyPluginCommand {
+ *     ...
+ *
+ *     @SubCommand
+ *     public static StickyPluginCommand subCommandA = new StickyPluginCommand("a", YourPlugin.getInstance()) {
+ *         ...
+ *     };
+ * }
+ * }</pre>
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SubCommand {}
