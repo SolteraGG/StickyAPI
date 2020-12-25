99aee7b67805a9431a608cb7a8e26be1ee81f9df
Zachery <zachery@foxboy.sh>
2020-12-25 16:11:30 -0600

    Update license headers

--- src/main/java/com/dumbdogdiner/stickyapi/bukkit/command/AsyncCommand.java
+++ src/main/java/com/dumbdogdiner/stickyapi/bukkit/command/AsyncCommand.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2020 DumbDogDiner <dumbdogdiner.com>. All rights reserved.
+ * Licensed under the MIT license, see LICENSE for more information...
+ */
 package com.dumbdogdiner.stickyapi.bukkit.command;
 
 import java.util.List;
