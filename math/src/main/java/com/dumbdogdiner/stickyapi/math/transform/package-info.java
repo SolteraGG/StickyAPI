fb3e29b2aae393b183841c56a543b51684e988e7
SkyezerFox <actuallyori@gmail.com>
2021-04-24 16:34:35 +0100

    wip :sparkles: add distribution methods

---
+++ math/src/main/java/com/dumbdogdiner/stickyapi/math/transform/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides implementations of mathematical matrices, used for translating vectors.
+ */
+package com.dumbdogdiner.stickyapi.math.transform;
---
+++ math/src/test/java/com/dumbdogdiner/stickyapi/math/stats/DistributionTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020-2021 DumbDogDiner <dumbdogdiner.com>. All rights reserved.
+ * Licensed under the MIT license, see LICENSE for more information...
+ */
+package com.dumbdogdiner.stickyapi.math.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DistributionTest {
+	static final double[] DATASET_X = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	static final double[] DATASET_Y = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+	@Test
+	void testExpectation() {
+		Assertions.assertEquals(5, Distribution.expectation(DATASET_X));
+		Assertions.assertEquals(5, Distribution.expectation(DATASET_Y));
+	}
+
+	@Test
+	void testVariance() {
+		Assertions.assertEquals(6.666666666666668, Distribution.variance(DATASET_X));
+		Assertions.assertEquals(6.666666666666668, Distribution.variance(DATASET_Y));
+	}
+
+	@Test
+	void testStandardDeviation() {
+		Assertions.assertEquals(2.5819888974716116, Distribution.standardDeviation(DATASET_X));
+		Assertions.assertEquals(2.5819888974716116, Distribution.standardDeviation(DATASET_Y));
+	}
+
+	@Test
+	void testPCC() {
+		Assertions.assertEquals(-1, Distribution.pcc(DATASET_X, DATASET_Y));
+	}
+}
