6ed6eafda670044449708eff11dfbe3eeafd260f
SkyezerFox <actuallyori@gmail.com>
2021-04-24 19:42:24 +0100

    compensate for floating point inaccuracy in TransformationBuilder test

--- math/src/test/java/com/dumbdogdiner/stickyapi/math/transform/TransformationBuilder2Test.java
+++ math/src/test/java/com/dumbdogdiner/stickyapi/math/transform/TransformationBuilder2Test.java
@@ -4,6 +4,7 @@
  */
 package com.dumbdogdiner.stickyapi.math.transform;
 
+import com.dumbdogdiner.stickyapi.math.NumberUtil;
 import com.dumbdogdiner.stickyapi.math.vector.Vector2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -39,6 +40,24 @@ class TransformationBuilder2Test {
 	void testRotationTransformation() {
 		TransformationBuilder2 builder = new TransformationBuilder2()
 			.apply(Matrix2.getRotationMatrix(Math.PI));
-		Assertions.assertEquals(new Vector2(-1, -1), builder.transform(new Vector2(1, 1)));
+		// translate vectors
+		Vector2 original = new Vector2(1, 1);
+		Vector2 translated = builder.transform(original);
+		// due to floating point inaccuracy
+		Assertions.assertTrue(NumberUtil.almostEquals(-1, translated.getX()));
+		Assertions.assertTrue(NumberUtil.almostEquals(-1, translated.getY()));
+	}
+
+	@Test
+	void testCompositeTransformation() {
+		TransformationBuilder2 builder = new TransformationBuilder2()
+			.apply(Matrix2.getEnlargementMatrix(2))
+			.apply(Matrix2.getRotationMatrix(Math.PI));
+		// translate vectors
+		Vector2 original = new Vector2(1, 1);
+		Vector2 translated = builder.transform(original);
+		// due to floating point inaccuracy
+		Assertions.assertTrue(NumberUtil.almostEquals(-2, translated.getX()));
+		Assertions.assertTrue(NumberUtil.almostEquals(-2, translated.getY()));
 	}
 }
