0508ec72ff1a011f30036ed375cc6364d6ca4f30
Andrew Katz <aakatz3@gmail.com>
2021-06-09 10:32:36 -0600

    format

--- luckperms/src/main/java/com/dumbdogdiner/stickyapi/luckperms/LuckpermsUtils.java
+++ luckperms/src/main/java/com/dumbdogdiner/stickyapi/luckperms/LuckpermsUtils.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2020-2021 DumbDogDiner <dumbdogdiner.com>. All rights reserved.
+ * Licensed under the MIT license, see LICENSE for more information...
+ */
 package com.dumbdogdiner.stickyapi.luckperms;
 
 import com.dumbdogdiner.stickyapi.common.ServerVersion;
@@ -54,4 +58,6 @@ public class LuckpermsUtils {
                 )
                 .collect(Collectors.toList());
     }
+
+
 }
